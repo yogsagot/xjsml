63f7f92841a01f5f13ad485d08254227aa01138f
yogsagot <yogsagot@debian.lan>
2020-03-28 18:49:38 +0200

    ...trippledot params

--- express/routes/index.js
+++ express/routes/index.js
@@ -4,9 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var title = 'Title from app';
-  var arr = [111, "333", "fdsf", 34.44];
-  var func = 'return "abc";';
-  res.render('index', { title: title, arr: arr, func: func });
+  var params = {id: "myid", class: title, required:true};
+  res.render('index', { title: title, params: params });
 });
 
 module.exports = router;
