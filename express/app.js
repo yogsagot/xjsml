8ac5a70e8e81ea7b4943b96072ef70d1192d70d8
yogsagot <yogsagot@debian.lan>
2020-03-28 09:07:53 +0200

    constructor params and caching

--- express/app.js
+++ express/app.js
@@ -9,7 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-const xjsml = new (require('..'))();
+const xjsml = new (require('..'))({
+  cacheEnabled: true
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
