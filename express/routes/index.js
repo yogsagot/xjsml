632803b67cc629500c996bd9028f39b13d1791c9
yogsagot <yogsagot@debian.lan>
2020-03-28 06:25:05 +0200

    trim modifier

--- express/routes/index.js
+++ express/routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var title = `this is 
-  <b>bold</b>`;
+  var title = `
+  
+  this is 
+
+  <b>bold</b>    
+  
+  `;
   res.render('index', { title: title, val: 55, obj: {a: 'aaa', b: 555} });
 });
 
