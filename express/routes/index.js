3db3e7df98db3e98d7d4f66c8afc2e7f745a0b44
yogsagot <yogsagot@debian.lan>
2020-03-28 05:51:18 +0200

    if when display json modifiers

--- express/routes/index.js
+++ express/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var title = 'this is <b>bold</b>';
-  res.render('index', { title: title, val: 55 });
+  res.render('index', { title: title, val: 55, obj: {a: 'aaa', b: 555} });
 });
 
 module.exports = router;
