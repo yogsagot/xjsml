9a5f425383257d552f2ba11da0bd110f7787ae1d
Juris Krumgolds <juris.krumgolds@gmail.com>
2022-04-20 04:44:00 +0300

    test if else tag

---
+++ test/tags/05 ifelse.tests.js
@@ -0,0 +1,124 @@
+var XJSML = require("../../lib/xjsml");
+var assert = require("chai").assert;
+const fs = require("fs");
+const path = require("path");
+
+describe("tags", () => {
+    describe("if else", () => {
+        const xjsml = new XJSML({
+            cacheEnabled: false,
+            caseSensitive: true,
+            javascriptTagAllowed: false,
+            callbacksAllowed: true,
+        });
+
+        it("01 if true", () => {
+            var templatename = "05 ifelse/01 if";
+            var filename = "05 ifelse/01 if true";
+            var args = {boolvar: true};
+            var rendered = xjsml.renderFile(fileName(`${templatename}.xjsml`), args);
+            var html = loadfile(`${filename}.html`);
+
+            assert.equal(rendered, html);
+        });
+
+        it("02 if false", () => {
+            var templatename = "05 ifelse/01 if";
+            var filename = "05 ifelse/02 if false";
+            var args = {boolvar: false};
+            var rendered = xjsml.renderFile(fileName(`${templatename}.xjsml`), args);
+            var html = loadfile(`${filename}.html`);
+
+            assert.equal(rendered, html);
+        });
+
+        it("03 if else true", () => {
+            var templatename = "05 ifelse/03 if else";
+            var filename = "05 ifelse/03 if else true";
+            var args = {boolvar: true};
+            var rendered = xjsml.renderFile(fileName(`${templatename}.xjsml`), args);
+            var html = loadfile(`${filename}.html`);
+
+            assert.equal(rendered, html);
+        });
+
+        it("04 if else false", () => {
+            var templatename = "05 ifelse/03 if else";
+            var filename = "05 ifelse/04 if else false";
+            var args = {boolvar: false};
+            var rendered = xjsml.renderFile(fileName(`${templatename}.xjsml`), args);
+            var html = loadfile(`${filename}.html`);
+
+            assert.equal(rendered, html);
+        });
+
+        it("05 elseif option 1", () => {
+            var templatename = "05 ifelse/05 elseif";
+            var filename = "05 ifelse/05 elseif option 1";
+            var args = {intvar: 1};
+            var rendered = xjsml.renderFile(fileName(`${templatename}.xjsml`), args);
+            var html = loadfile(`${filename}.html`);
+
+            assert.equal(rendered, html);
+        });
+
+        it("06 elseif option 2", () => {
+            var templatename = "05 ifelse/05 elseif";
+            var filename = "05 ifelse/06 elseif option 2";
+            var args = {intvar: 2};
+            var rendered = xjsml.renderFile(fileName(`${templatename}.xjsml`), args);
+            var html = loadfile(`${filename}.html`);
+
+            assert.equal(rendered, html);
+        });
+
+        it("07 elseif option 3", () => {
+            var templatename = "05 ifelse/05 elseif";
+            var filename = "05 ifelse/07 elseif option 3";
+            var args = {intvar: 3};
+            var rendered = xjsml.renderFile(fileName(`${templatename}.xjsml`), args);
+            var html = loadfile(`${filename}.html`);
+
+            assert.equal(rendered, html);
+        });
+
+        it("08 elseif option 4", () => {
+            var templatename = "05 ifelse/05 elseif";
+            var filename = "05 ifelse/08 elseif option 4";
+            var args = {intvar: 4};
+            var rendered = xjsml.renderFile(fileName(`${templatename}.xjsml`), args);
+            var html = loadfile(`${filename}.html`);
+
+            assert.equal(rendered, html);
+        });
+
+        it("09 elseif option 5", () => {
+            var templatename = "05 ifelse/05 elseif";
+            var filename = "05 ifelse/09 elseif option 5";
+            var args = {intvar: 5};
+            var rendered = xjsml.renderFile(fileName(`${templatename}.xjsml`), args);
+            var html = loadfile(`${filename}.html`);
+
+            assert.equal(rendered, html);
+        });
+
+        it("10 elseif else", () => {
+            var templatename = "05 ifelse/05 elseif";
+            var filename = "05 ifelse/10 elseif else";
+            var args = {intvar: -1};
+            var rendered = xjsml.renderFile(fileName(`${templatename}.xjsml`), args);
+            var html = loadfile(`${filename}.html`);
+
+            assert.equal(rendered, html);
+        });
+    });
+});
+
+function fileName(filename) {
+    return path.join(__dirname, filename);
+}
+
+function loadfile(filename) {
+    return fs.readFileSync(fileName(filename), "utf8").toString();
+}
+
